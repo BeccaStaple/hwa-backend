8cbce0bf043d11055c404768a1391fd641c5366c
Becca Staple <petite.becca.s@gmail.com>
2020-06-26 14:19:42 +0100

    test working for create in stamp service

--- src/test/java/com/qa/hwa/service/StampServiceUnitTest.java
+++ src/test/java/com/qa/hwa/service/StampServiceUnitTest.java
@@ -1,5 +1,45 @@
 package com.qa.hwa.service;
 
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+import org.junit.Before;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.mockito.InjectMocks;
+import org.mockito.Mock;
+import org.mockito.Mockito;
+import org.mockito.junit.MockitoJUnitRunner;
+
+import com.qa.hwa.persistence.domain.Stamp;
+import com.qa.hwa.persistence.repo.StampRepo;
+
+@RunWith(MockitoJUnitRunner.class)
 public class StampServiceUnitTest {
 
+	private final Stamp STAMP = new Stamp("star", 1.11, 1996);
+	
+	private Stamp savedStamp;
+	
+	@Mock
+	private StampRepo repo;
+	
+	@InjectMocks
+	private StampService service;
+	
+	@Before
+	public void init() {
+		this.savedStamp = new Stamp(STAMP.getName(), STAMP.getValue(), STAMP.getYearMade());
+		this.savedStamp.setId(1L);
+	}
+	
+	@Test
+	public void testCreate() {
+		Mockito.when(this.repo.save(STAMP)).thenReturn(savedStamp);
+		
+		assertEquals(savedStamp, service.create(STAMP));
+		
+		Mockito.verify(this.repo, Mockito.times(1)).save(STAMP);
+	}
+	
 }

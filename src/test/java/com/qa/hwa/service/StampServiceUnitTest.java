7dc23b5ea89db5ed62b3fe26d2f43d34238a00f0
Becca Staple <petite.becca.s@gmail.com>
2020-07-08 14:44:22 +0100

    update test now working for stamp service

--- src/test/java/com/qa/hwa/service/StampServiceUnitTest.java
+++ src/test/java/com/qa/hwa/service/StampServiceUnitTest.java
@@ -1,6 +1,5 @@
 package com.qa.hwa.service;
 
-
 import static org.junit.Assert.assertFalse;
 import static org.junit.jupiter.api.Assertions.assertEquals;
 
@@ -25,89 +24,100 @@ import com.qa.hwa.persistence.repo.StampRepo;
 public class StampServiceUnitTest {
 
 	private final Stamp STAMP = new Stamp("star", 1.11, 1996);
-	
+
 	private Stamp savedStamp;
-	
+
 	private StampDto stampDto;
-	
+
+	private final long ID = 1L;
+
 	@Mock
 	private StampRepo repo;
-	
+
 	@Mock
 	private ModelMapper mapper;
-	
+
 	@InjectMocks
 	private StampService service;
-	
+
 	@Before
 	public void init() {
 		this.savedStamp = new Stamp(STAMP.getName(), STAMP.getValue(), STAMP.getYearMade());
 		this.savedStamp.setId(1L);
-		this.stampDto = new ModelMapper().map(savedStamp,  StampDto.class);
+		this.stampDto = service.mapToDto(savedStamp);
 	}
-	
+
 	@Test
 	public void testCreate() {
-		
+
 		Mockito.when(this.repo.save(STAMP)).thenReturn(savedStamp);
-		Mockito.when(this.mapper.map(savedStamp, StampDto.class)).thenReturn(stampDto);
-		
+		Mockito.when(service.mapToDto(savedStamp)).thenReturn(stampDto);
+
 		StampDto createdStampDto = this.stampDto;
-		
+
 		assertEquals(createdStampDto, service.create(STAMP));
-		
+
 		Mockito.verify(this.repo, Mockito.times(1)).save(STAMP);
 	}
-	
+
 	@Test
 	public void testRead() {
 		List<Stamp> stampList = new ArrayList<Stamp>();
-		
+
 		stampList.add(savedStamp);
-		
+
 		Mockito.when(this.repo.findAll()).thenReturn(stampList);
-		Mockito.when(this.mapper.map(savedStamp, StampDto.class)).thenReturn(stampDto);
-		
+		Mockito.when(service.mapToDto(savedStamp)).thenReturn(stampDto);
+
 		assertFalse(service.read().isEmpty());
-		
+
 		Mockito.verify(this.repo, Mockito.times(1)).findAll();
-		
+
 	}
+
+	//TODO
+
+//	@Test
+//	public void testReadOne() {
+//		Mockito.when(this.repo.findById(this.ID))
+//		
+//	}
+
 	
-	//need test for readOne()
-	
+	//TODO
 	@Test
 	public void testUpdate() {
-		
-		Mockito.when(this.repo.findById(savedStamp.getId())).thenReturn(Optional.of(savedStamp));
-	
+
 		Stamp newStamp = new Stamp("updated stamp", 1.99, 1900);
-		
-		Stamp stampPlusId = new Stamp("updated stamp", 1.99, 1900);
+
+		Stamp stampPlusId = new Stamp(newStamp.getName(), newStamp.getValue(), newStamp.getYearMade());
 		stampPlusId.setId(savedStamp.getId());
-		
+
 		StampDto stampToDtoID = service.mapToDto(stampPlusId);
+
+		Mockito.when(this.repo.findById(savedStamp.getId())).thenReturn(Optional.of(savedStamp));
+
+		Mockito.when(this.service.mapToDto(stampPlusId)).thenReturn(stampToDtoID);
 		
 		Mockito.when(this.repo.save(stampPlusId)).thenReturn(stampPlusId);
+
 		
-		assertEquals(stampPlusId, this.service.update(newStamp, savedStamp.getId()));
-		
-		Mockito.verify(this.repo, Mockito.times(1)).findById(savedStamp.getId());
+		assertEquals(stampToDtoID, this.service.update(newStamp, savedStamp.getId()));
+
+		Mockito.verify(this.repo, Mockito.times(1)).findById(ID);
 		Mockito.verify(this.repo, Mockito.times(1)).save(stampPlusId);
 	}
-	
+
 	@Test
 	public void testDelete() {
-		final long ID = 1L;
+
 		final boolean RESULT = false;
-		
+
 		Mockito.when(this.repo.existsById(ID)).thenReturn(false);
-		
+
 		assertEquals(RESULT, this.service.delete(1L));
-		
+
 		Mockito.verify(this.repo, Mockito.times(1)).existsById(ID);
 	}
-	
-	
-	
+
 }

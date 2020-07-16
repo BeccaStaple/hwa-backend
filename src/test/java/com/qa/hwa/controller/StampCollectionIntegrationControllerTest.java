42f9bf2c6141c80658c017c799eb8bbf4cc92f6a
Becca Staple <petite.becca.s@gmail.com>
2020-07-16 12:25:02 +0100

    readOne test working for collectoin and stamp

--- src/test/java/com/qa/hwa/controller/StampCollectionIntegrationControllerTest.java
+++ src/test/java/com/qa/hwa/controller/StampCollectionIntegrationControllerTest.java
@@ -1,6 +1,127 @@
 package com.qa.hwa.controller;
 
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.delete;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.post;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.put;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.content;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import org.junit.Before;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.modelmapper.ModelMapper;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.boot.test.context.SpringBootTest.WebEnvironment;
+import org.springframework.http.MediaType;
+import org.springframework.test.context.junit4.SpringRunner;
+import org.springframework.test.web.servlet.MockMvc;
+
+import com.fasterxml.jackson.core.JsonProcessingException;
+import com.fasterxml.jackson.databind.ObjectMapper;
+import com.qa.hwa.dto.StampCollectionDto;
+import com.qa.hwa.persistence.domain.StampCollection;
+import com.qa.hwa.persistence.repo.StampCollectionRepo;
+import com.qa.hwa.service.StampCollectionService;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest(webEnvironment = WebEnvironment.RANDOM_PORT)
+@AutoConfigureMockMvc
 public class StampCollectionIntegrationControllerTest {
 
+	private StampCollection collection;
+
+	private StampCollection savedCollection;
+
+	private StampCollectionDto collectionDto;
+
+	private long id;
+
+	private List<StampCollectionDto> listCollectionDto;
+
+	@Autowired
+	private StampCollectionService service;
+
+	@Autowired
+	private StampCollectionRepo repo;
+
+	@Autowired
+	private MockMvc mockMvc;
+
+	@Autowired
+	private ObjectMapper mapper;
+
+	@Autowired
+	private ModelMapper modelMapper;
+
+	@Before
+	public void init() {
+		this.id = 1L;
+		this.collection = new StampCollection("test collection", 12.34);
+		this.savedCollection = new StampCollection(collection.getTheme(), collection.getValue());
+		this.savedCollection.setId(this.id);
+		this.service = new StampCollectionService(repo, modelMapper);
+		this.collectionDto = service.mapToDto(savedCollection);
+		this.listCollectionDto = new ArrayList<StampCollectionDto>();
+	}
+
+	@Test
+	public void testCreate() throws JsonProcessingException, Exception {
+		this.mockMvc
+				.perform(post("/collection/create").contentType(MediaType.APPLICATION_JSON)
+						.accept(MediaType.APPLICATION_JSON).content(this.mapper.writeValueAsString(collectionDto)))
+				.andExpect(status().isCreated())
+				.andExpect(content().json(this.mapper.writeValueAsString(collectionDto)));
+
+	}
+
+	@Test
+	public void testRead() throws JsonProcessingException, Exception {
+		this.mockMvc
+				.perform(get("/collection/read").contentType(MediaType.APPLICATION_JSON)
+						.accept(MediaType.APPLICATION_JSON))
+				.andExpect(status().isOk())
+				.andExpect(content().json(this.mapper.writeValueAsString(listCollectionDto)));
+
+	}
+
+	@Test
+	public void testReadOne() throws JsonProcessingException, Exception {
+		this.service.create(savedCollection);
+
+		this.mockMvc
+				.perform(get("/collection/read/" + this.id).contentType(MediaType.APPLICATION_JSON)
+						.accept(MediaType.APPLICATION_JSON))
+				.andExpect(status().isOk()).andExpect(content().json(this.mapper.writeValueAsString(collectionDto)));
+
+	}
 	
+	@Test
+	public void testUpdate() throws JsonProcessingException, Exception {
+		this.service.create(savedCollection);
+		
+		StampCollection testCollection = new StampCollection("updated collection", 33.99);
+
+		StampCollection updateCollection = new StampCollection(testCollection.getTheme(), testCollection.getValue());
+		updateCollection.setId(this.id);
+
+		StampCollectionDto colToDto = this.service.mapToDto(updateCollection);
+
+		this.mockMvc
+				.perform(put("/collection/update/" + this.id).contentType(MediaType.APPLICATION_JSON).accept(MediaType.APPLICATION_JSON))
+				.andExpect(status().isAccepted()).andExpect(content().json(this.mapper.writeValueAsString(colToDto)));
+	}
+	
+	@Test
+	public void testDelete() throws Exception {
+		this.mockMvc.perform(delete("/collection/delete/" + this.id).contentType(MediaType.APPLICATION_JSON)
+				.accept(MediaType.APPLICATION_JSON)).andExpect(status().isNoContent());
+	}
+	
+
 }
--- src/test/java/com/qa/hwa/controller/StampControllerIntegrationTest.java
+++ src/test/java/com/qa/hwa/controller/StampControllerIntegrationTest.java
@@ -26,24 +26,32 @@ import com.fasterxml.jackson.core.JsonProcessingException;
 import com.fasterxml.jackson.databind.ObjectMapper;
 import com.qa.hwa.dto.StampDto;
 import com.qa.hwa.persistence.domain.Stamp;
+import com.qa.hwa.persistence.repo.StampRepo;
+import com.qa.hwa.service.StampService;
 
 @RunWith(SpringRunner.class)
 @SpringBootTest(webEnvironment = WebEnvironment.RANDOM_PORT)
 @AutoConfigureMockMvc
 public class StampControllerIntegrationTest {
 
-	@Autowired
-	private MockMvc mockMvc;
-
 	private Stamp stamp;
 
 	private Stamp savedStamp;
 
 	private StampDto stampDto;
-	
+
 	private long id;
-	
+
 	private List<StampDto> listStampDto;
+	
+	@Autowired
+	private StampRepo repo;
+	
+	@Autowired
+	private StampService service;
+
+	@Autowired
+	private MockMvc mockMvc;
 
 	@Autowired
 	private ObjectMapper mapper;
@@ -53,12 +61,13 @@ public class StampControllerIntegrationTest {
 
 	@Before
 	public void init() {
+		this.id = 1L;
 		this.stamp = new Stamp("integ test stamp", 23.45, 2011);
 		this.savedStamp = new Stamp(stamp.getName(), stamp.getValue(), stamp.getYearMade());
 		this.savedStamp.setId(this.id);
-		this.stampDto = this.modelMapper.map(savedStamp, StampDto.class);
+		this.service = new StampService(repo, modelMapper);
+		this.stampDto = service.mapToDto(savedStamp);
 		this.listStampDto = new ArrayList<StampDto>();
-		this.id = 1L;
 	}
 
 	@Test
@@ -76,37 +85,37 @@ public class StampControllerIntegrationTest {
 				.perform(get("/stamp/read").contentType(MediaType.APPLICATION_JSON).accept(MediaType.APPLICATION_JSON))
 				.andExpect(status().isOk()).andExpect(content().json(this.mapper.writeValueAsString(listStampDto)));
 	}
-	
+
 	@Test
 	public void testReadOne() throws JsonProcessingException, Exception {
+		this.service.create(savedStamp);
+		
 		this.mockMvc
-		.perform(get("/stamp/read/" + this.id)
-				.contentType(MediaType.APPLICATION_JSON)
-				.accept(MediaType.APPLICATION_JSON))
-		.andExpect(status().isOk())
-		.andExpect(content()
-				.json(this.mapper.writeValueAsString(stampDto)));
+				.perform(get("/stamp/read/" + this.id).contentType(MediaType.APPLICATION_JSON)
+						.accept(MediaType.APPLICATION_JSON))
+				.andExpect(status().isOk()).andExpect(content().json(this.mapper.writeValueAsString(stampDto)));
 	}
-	
-	@Test 
+
+	@Test
 	public void testUpdate() throws JsonProcessingException, Exception {
+		this.service.create(savedStamp);
 		
 		Stamp testStamp = new Stamp("updatedStamp", 33.99, 2020);
-		
+
 		Stamp updateStamp = new Stamp(testStamp.getName(), testStamp.getValue(), testStamp.getYearMade());
 		updateStamp.setId(this.id);
-		
-		StampDto stampToDo = this.modelMapper.map(updateStamp, StampDto.class);
-		
+
+		StampDto stampToDto = this.service.mapToDto(updateStamp);
+
 		this.mockMvc
-		.perform(put("/stamp/update/" + this.id).contentType(MediaType.APPLICATION_JSON)
-				.accept(MediaType.APPLICATION_JSON)).andExpect(status().isAccepted()).andExpect(content().json(this.mapper.writeValueAsString(stampToDo)));
+				.perform(put("/stamp/update/" + this.id).contentType(MediaType.APPLICATION_JSON).accept(MediaType.APPLICATION_JSON))
+				.andExpect(status().isAccepted()).andExpect(content().json(this.mapper.writeValueAsString(stampToDto)));
 	}
-	
+
 	@Test
 	public void testDelete() throws Exception {
-		this.mockMvc.perform(delete("/stamp/delete/" + this.id).contentType(MediaType.APPLICATION_JSON).accept(MediaType.APPLICATION_JSON))
-		.andExpect(status().isNoContent());
+		this.mockMvc.perform(delete("/stamp/delete/" + this.id).contentType(MediaType.APPLICATION_JSON)
+				.accept(MediaType.APPLICATION_JSON)).andExpect(status().isNoContent());
 	}
 
 }
